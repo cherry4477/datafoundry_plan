1c154b846c7ca160ba913eb22cc0b425718cc165
xmwilldo <xmwillgo@163.com>
2016-10-21 14:59:39 +0800

    modify query list by region

--- models/plan_model.go
+++ models/plan_model.go
@@ -248,7 +248,7 @@ func QueryPlans(db *sql.DB, region, ptype, orderBy string, sortOrder bool, offse
 }
 
 func getRegionId(db *sql.DB, region string) (int, error) {
-	sql := `SELECT ID FROM DF_PLAN_REGION WHERE IDENTIFICATION=?`
+	sql := `SELECT ID FROM DF_PLAN_REGION WHERE REGION=?`
 
 	row := db.QueryRow(sql, region)
 
